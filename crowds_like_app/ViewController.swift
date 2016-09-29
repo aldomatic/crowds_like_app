e90567a7fe725804b12132dc1b68971051967c37
Aldo Lugo <aldo@aldomatic.com>
2016-09-29 11:15:35 -0500

    Initial Commit

---
+++ crowds_like_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crowds_like_app
+//
+//  Created by Aldo Lugo on 9/29/16.
+//  Copyright © 2016 Aldo Lugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
